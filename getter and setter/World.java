49411c6819e2e77abad52612f08958eb5b8f3c47
pragathi r <pragathirajshekar@gmail.com>
2023-10-28 21:50:11 +0530

    getter and setter

---
+++ getter and setter/World.java
@@ -0,0 +1,413 @@
+package com.xworkz.september.things;
+
+public class World {
+	private String entity;
+	private String simpleObject;
+	private String scientificCosmology;
+	private String theoriesOfModality;
+	private String phenomenology;
+	private String theology;
+	private String religion;
+	private String eschatology;
+	private String interdependent;
+	private String contrasted;
+	private String earth;
+	private String worldHistory;
+	private String worldPolitics;
+	private String worldLanguage;
+	private String worldGovernment;
+	private String worldWar;
+	private String worldPopulation;
+	private String worldEconomy;
+	private String worldChampionship;
+	private String grossDomesticProduct;
+	private String populationGrowthRate;
+	private String worldMap;
+	private String waterWorld;
+	private String naturalHistory;
+	private String geophysicalCharacteristics;
+	private String WHO;
+	private String universalHealth;
+	private String healthEmergencies;
+	private String civiSociety;
+	private String organizations;
+	private String foundations;
+	private String researchers;
+	private String advocates;
+	private String constitution;
+	private String principles;
+	private String assembly;
+	private String healthAssembly;
+	private String memberStates;
+	private String accountability;
+	private String worldResources;
+	private String transformation;
+	private String collaboration;
+	private String communities;
+	private String multiculturalEnvironment;
+	private String humanRights;
+	private String diverseNature;
+	private String populationDay;
+	private String globalLifeSpans;
+	private String urbanization;
+	private String acceleratingMigration;
+    
+	public World() {
+		System.out.println("............................................");
+	}
+	public void setEntity(String entity) {
+		this.entity=entity;
+	}
+	public String getEntity() {
+	     return this.entity;
+	}
+	
+	public void setSimpleObject(String simpleObject) {
+		this.simpleObject=simpleObject;
+	}
+	public String getSimpleObject() {
+		return this.simpleObject;
+	}
+	
+	public void setScientificCosmology(String scientificCosmology) {
+		this.scientificCosmology = scientificCosmology;
+	}
+	public String getScientificCosmology() {
+		return this.scientificCosmology;
+	}
+	
+	public void setTheoriesOfModality(String theoriesOfModality) {
+		this.theoriesOfModality = theoriesOfModality;
+	}
+	public String getTheoriesOfModality() {
+		return this.theoriesOfModality;
+	}
+	
+	public void setPhenomenology(String phenomenology) {
+		this.phenomenology = phenomenology;
+	}
+	public String getPhenomenology() {
+		return this.phenomenology;
+	}
+	
+	public void setTheology(String theology) {
+		this.theology = theology;
+	}
+	public String getTheology() {
+		return this.theology;
+	}
+
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+	public String getReligion() {
+		return this.religion;
+	}
+
+	public void setEschatology(String eschatology) {
+		this.eschatology = eschatology;
+	}
+	public String getEschatology() {
+		return this.eschatology;
+	}
+
+	public void setInterdependent(String interdependent) {
+		this.interdependent = interdependent;
+	}
+	public String getInterdependent() {
+		return this.interdependent;
+	}
+
+	public void setContrasted(String contrasted) {
+		this.contrasted = contrasted;
+	}
+	public String getContrasted() {
+		return this.contrasted;
+	}
+
+	public void setEarth(String earth) {
+		this.earth=earth;
+	}
+	public String getEarth() {
+		return this.earth;
+	}
+
+	public void setWorldHistory(String worldHistory) {
+		this.worldHistory=worldHistory;
+	}
+	public String getWorldHistory() {
+		return this.worldHistory;
+	}
+
+	public void setWorldPolitics(String worldPolitics) {
+		this.worldPolitics = worldPolitics;
+	}
+	public String getWorldPolitics() {
+		return this.worldPolitics;
+	}
+
+	public void setWorldLanguage(String worldLanguage) {
+		this.worldLanguage = worldLanguage;
+	}
+	public String getWorldLanguage() {
+		return this.worldLanguage;
+	}
+
+	public void setWorldGovernment(String worldGovernment) {
+		this.worldGovernment = worldGovernment;
+	}
+	public String getWorldGovernment() {
+		return this.worldGovernment;
+	}
+
+	public void setWorldWar(String worldWar) {
+		this.worldWar = worldWar;
+	}
+	public String getWorldWar() {
+		return this.worldWar;
+	}
+
+	public void setWorldPopulation(String worldPopulation) {
+		this.worldPopulation = worldPopulation;
+	}
+	public String getWorldPopulation() {
+		return this.worldPopulation;
+	}
+	
+	public void setWorldEconomy(String worldEconomy) {
+		this.worldEconomy = worldEconomy;
+	}
+	public String getWorldEconomy(){
+		return this.worldEconomy;
+	}
+	
+	public void setWorldChampionship(String worldChampionship) {
+		this.worldChampionship = worldChampionship;
+	}
+	public String getWorldChampionship(){
+		return this.worldChampionship;
+	}
+	
+	public void setGrossDomesticProduct(String grossDomesticProduct) {
+		this.grossDomesticProduct = grossDomesticProduct;
+	}
+	public String getGrossDomesticProduct(){
+		return this.grossDomesticProduct;
+	}
+	
+	public void setPopulationGrowthRate(String populationGrowthRate) {
+		this.populationGrowthRate = populationGrowthRate;
+	}
+	public String getPopulationGrowthRate(){
+		return this.populationGrowthRate;
+	}
+	
+	public void setWorldMap(String worldMap) {
+		this.worldMap = worldMap;
+	}
+	public String getWorldMap(){
+		return this.worldMap;
+	}
+	
+	public void setWaterWorld(String waterWorld) {
+		this.waterWorld = waterWorld;
+	}
+	public String getWaterWorld(){
+		return this.waterWorld;
+	}
+	
+	public void setNaturalHistory(String naturalHistory) {
+		this.naturalHistory = naturalHistory;
+	}
+	public String getNaturalHistory(){
+		return this.naturalHistory;
+	}
+	
+	public void setGeophysicalCharacteristics(String geophysicalCharacteristics) {
+		this.geophysicalCharacteristics = geophysicalCharacteristics;
+	}
+	public String getGeophysicalCharacteristics(){
+		return this.geophysicalCharacteristics;
+	}
+	
+	public void setWHO(String WHO) {
+		this.WHO = WHO;
+	}
+	public String getWHO(){
+		return this.WHO;
+	}
+	
+	public void setUniversalHealth(String universalHealth) {
+		this.universalHealth = universalHealth;
+	}
+	public String getUniversalHealth(){
+		return this.universalHealth;
+	}
+	
+	public void setHealthEmergencies(String healthEmergencies) {
+		this.healthEmergencies = healthEmergencies;
+	}
+	public String getHealthEmergencies(){
+		return this.healthEmergencies;
+	}
+	
+	public void setCiviSociety(String civiSociety) {
+		this.civiSociety = civiSociety;
+	}
+	public String getCiviSociety(){
+		return this.civiSociety;
+	}
+	
+	public void setOrganizations(String organizations) {
+		this.organizations = organizations;
+	}
+	public String getOrganizations(){
+		return this.organizations;
+	}
+	
+	public void setFoundations(String foundations) {
+		this.foundations = foundations;
+	}
+	public String getFoundations(){
+		return this.foundations;
+	}
+	
+	public void setResearchers(String researchers) {
+		this.researchers = researchers;
+	}
+	public String getResearchers(){
+		return this.researchers;
+	}
+	
+	public void setAdvocates(String advocates) {
+		this.advocates = advocates;
+	}
+	public String getAdvocates(){
+		return this.advocates;
+	}
+	
+	public void setConstitution(String constitution) {
+		this.constitution = constitution;
+	}
+	public String getConstitution(){
+		return this.constitution;
+	}
+	
+	public void setPrinciples(String principles) {
+		this.principles = principles;
+	}
+	public String getPrinciples(){
+		return this.principles;
+	}
+	
+	public void setAssembly(String assembly) {
+		this.assembly = assembly;
+	}
+	public String getAssembly(){
+		return this.assembly;
+	}
+	
+	public void setHealthAssembly(String healthAssembly) {
+		this.healthAssembly = healthAssembly;
+	}
+	public String getHealthAssembly(){
+		return this.healthAssembly;
+	}
+	
+	public void setMemberStates(String memberStates) {
+		this.memberStates = memberStates;
+	}
+	public String getMemberStates(){
+		return this.memberStates;
+	}
+	
+	public void setAccountability(String accountability) {
+		this.accountability = accountability;
+	}
+	public String getAccountability(){
+		return this.accountability;
+	}
+	
+	public void setWorldResources(String worldResources) {
+		this.worldResources = worldResources;
+	}
+	public String getWorldResources(){
+		return this.worldResources;
+	}
+	
+	public void setTransformation(String transformation) {
+		this.transformation = transformation;
+	}
+	public String getTransformation(){
+		return this.transformation;
+	}
+	
+	public void setCollaboration(String collaboration) {
+		this.collaboration = collaboration;
+	}
+	public String getCollaboration(){
+		return this.collaboration;
+	}
+	
+	public void setCommunities(String communities) {
+		this.communities = communities;
+	}
+	public String getCommunities(){
+		return this.communities;
+	}
+	
+	public void setMulticulturalEnvironment(String multiculturalEnvironment) {
+		this.multiculturalEnvironment = multiculturalEnvironment;
+	}
+	public String getMulticulturalEnvironment(){
+		return this.multiculturalEnvironment;
+	}
+	
+	public void setHumanRights(String humanRights) {
+		this.humanRights = humanRights;
+	}
+	public String getHumanRights(){
+		return this.humanRights;
+	}
+	
+	public void setDiverseNature(String diverseNature) {
+		this.diverseNature = diverseNature;
+	}
+	public String getDiverseNature(){
+		return this.diverseNature;
+	}
+	
+	public void setPopulationDay(String populationDay) {
+		this.populationDay = populationDay;
+	}
+	public String getPopulationDay(){
+		return this.populationDay;
+	}
+	
+	public void setGlobalLifeSpans(String globalLifeSpans) {
+		this.globalLifeSpans = globalLifeSpans;
+	}
+	public String getGlobalLifeSpans(){
+		return this.globalLifeSpans;
+	}
+	
+	public void setUrbanization(String urbanization) {
+		this.urbanization = urbanization;
+	}
+	public String getUrbanization(){
+		return this.urbanization;
+	}
+	
+	public void setAcceleratingMigration(String acceleratingMigration) {
+		this.acceleratingMigration = acceleratingMigration;
+	}
+	public String getAcceleratingMigration(){
+		return this.acceleratingMigration;
+	}
+	
+	
+
+	      
+	
+
+}
---
+++ getter and setter/WorldRunner.java
@@ -0,0 +1,211 @@
+package com.xworkz.september.driver;
+
+import com.xworkz.september.things.World;
+
+public class WorldRunner {
+	public static void main(String[] args) {
+		World world = new World();
+		
+		world.setEntity("World");
+		String entity = world.getEntity();
+		System.out.println(entity);
+		
+		world.setSimpleObject("objects");
+		String simpleObject = world.getSimpleObject();
+		System.out.println(simpleObject);
+		
+		world.setScientificCosmology("cosmology");
+		String scientificCosmology = world.getScientificCosmology();
+		System.out.println(scientificCosmology);
+		
+		world.setTheoriesOfModality("modality");
+		String theoriesOfModality = world.getTheoriesOfModality();
+		System.out.println(theoriesOfModality);
+		
+		world.setPhenomenology("phenomenology");
+		String phenomenology = world.getPhenomenology();
+		System.out.println(phenomenology);
+		
+		world.setTheology("theology");
+		String theology = world.getTheology();
+		System.out.println(theology);
+		
+		world.setReligion("religion");
+		String religion= world.getReligion();
+		System.out.println(religion);
+		
+		world.setEschatology("eschatology");
+		String eschatology = world.getEschatology();
+		System.out.println(eschatology);
+		
+		world.setInterdependent("interdependent");
+		String interdependent = world.getInterdependent();
+		System.out.println(interdependent);
+		
+		world.setEarth("earth");
+		String earth= world.getEarth();
+		System.out.println(earth);
+		
+		world.setContrasted("contrasted");
+		String contrasted = world.getContrasted();
+		System.out.println(contrasted);
+		
+		world.setWorldHistory("history of world");
+		String worldHistory = world.getWorldHistory();
+		System.out.println(worldHistory);
+		
+		world.setWorldPolitics("politics");
+		String worldPolitics = world.getWorldPolitics();
+		System.out.println(worldPolitics);
+		
+		world.setWorldLanguage("languages");
+		String worldLanguage = world.getWorldLanguage();
+		System.out.println( worldLanguage);
+		
+		world.setWorldGovernment("government");
+		String worldGovernment = world.getWorldGovernment();
+		System.out.println(worldGovernment);
+		
+		world.setWorldWar("world war");
+		String worldWar = world.getWorldWar();
+		System.out.println(worldWar);
+		
+		world.setWorldPopulation("population");
+		String worldPopulation = world.getWorldPopulation();
+		System.out.println(worldPopulation);
+		
+		world.setWorldEconomy("economy");
+		String worldEconomy = world.getWorldEconomy();
+		System.out.println(worldEconomy);
+		
+		world.setWorldChampionship("championship");
+		String worldChampionship = world.getWorldChampionship();
+		System.out.println(worldChampionship);
+		
+		world.setGrossDomesticProduct("domestic product");
+		String grossDomesticProduct = world.getGrossDomesticProduct();
+		System.out.println(grossDomesticProduct);
+		
+		world.setPopulationGrowthRate("population growth rate");
+		String populationGrowthRate = world.getPopulationGrowthRate();
+		System.out.println(populationGrowthRate);
+		
+		world.setWorldMap("world map");
+		String worldMap = world.getWorldMap();
+		System.out.println(worldMap);
+		
+		world.setWaterWorld("water world");
+		String waterWorld = world.getWaterWorld();
+		System.out.println(waterWorld);
+		
+		world.setNaturalHistory("natural history");
+		String naturalHistory = world.getNaturalHistory();
+		System.out.println(naturalHistory);
+		
+		world.setGeophysicalCharacteristics("geographysical characteristics");
+		String geophysicalCharacteristics = world.getGeophysicalCharacteristics();
+		System.out.println(geophysicalCharacteristics);
+		
+		world.setWHO("world health organization");
+		String WHO = world.getWHO();
+		System.out.println(WHO);
+		
+		world.setUniversalHealth("health");
+		String universalHealth = world.getUniversalHealth();
+		System.out.println(universalHealth);
+		
+		world.setHealthEmergencies("health emergencies");
+		String healthEmergencies = world.getHealthEmergencies();
+		System.out.println(healthEmergencies);
+		
+		world.setCiviSociety("civil society");
+		String civiSociety = world.getCiviSociety();
+		System.out.println(civiSociety);
+		
+		world.setOrganizations("organizations");
+		String organizations = world.getOrganizations();
+		System.out.println(organizations);
+		
+		world.setFoundations("foundation");
+		String foundations = world.getFoundations();
+		System.out.println(foundations);
+		
+		world.setResearchers("researchers");
+		String researchers = world.getResearchers();
+		System.out.println(researchers);
+		
+		world.setAdvocates("law");
+		String advocates = world.getAdvocates();
+		System.out.println(advocates);
+		
+		world.setConstitution("constitution");
+		String constitution = world.getConstitution();
+		System.out.println(constitution);
+		
+		world.setPrinciples("principles");
+		String principles = world.getPrinciples();
+		System.out.println(principles);
+		
+		world.setAssembly("assembly");
+		String assembly = world.getAssembly();
+		System.out.println(assembly);
+		
+		world.setHealthAssembly("health assembly");
+		String healthAssembly = world.getHealthAssembly();
+		System.out.println(healthAssembly);
+		
+		world.setMemberStates("member states");
+		String memberStates = world.getMemberStates();
+		System.out.println(memberStates);
+		
+		world.setAccountability("accountability");
+		String accountability = world.getAccountability();
+		System.out.println(accountability);
+		
+		world.setWorldResources("resources");
+		String worldResources = world.getWorldResources();
+		System.out.println(worldResources);
+		
+		world.setTransformation("transformation");
+		String transformation = world.getTransformation();
+		System.out.println(transformation);
+		
+		world.setCollaboration("collaboration");
+		String collaboration = world.getCollaboration();
+		System.out.println(collaboration);
+		
+		world.setCommunities("communities");
+		String communities = world.getCommunities();
+		System.out.println(communities);
+		
+		world.setMulticulturalEnvironment("multi cultural environment");
+		String multiculturalEnvironment = world.getMulticulturalEnvironment();
+		System.out.println(multiculturalEnvironment);
+		
+		world.setHumanRights("human rights");
+		String humanRights = world.getHumanRights();
+		System.out.println(humanRights);
+		
+		world.setDiverseNature("nature is diverse");
+		String diverseNature = world.getDiverseNature();
+		System.out.println(diverseNature);
+		
+		world.setPopulationDay("population day");
+		String populationDay = world.getPopulationDay();
+		System.out.println(populationDay);
+		
+		world.setGlobalLifeSpans("globalization");
+		String globalLifeSpans = world.getGlobalLifeSpans();
+		System.out.println(globalLifeSpans);
+		
+		world.setUrbanization("urbanization");
+		String urbanization = world.getUrbanization();
+		System.out.println(urbanization);
+		
+		world.setAcceleratingMigration("migration");
+		String acceleratingMigration = world.getAcceleratingMigration();
+		System.out.println(acceleratingMigration);
+		
+	}
+
+}
